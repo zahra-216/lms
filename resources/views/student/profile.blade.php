153df6b83bda68e75e3ec2a8e35917dfca749a84
Fathima Zahra <zahrahasan2163@gmail.com>
2026-09-02 14:06:00 +0530

    check validation error

--- resources/views/student/profile.blade.php
+++ resources/views/student/profile.blade.php
@@ -403,6 +403,19 @@ footer.shift{
             <!-- TOP HEADER -->
             <div class="profile-top text-center text-white">
 
+                @if($errors->any())
+                    <div class="alert alert-danger">
+                        <ul class="mb-0">
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                @if(session('success'))
+                    <div class="alert alert-success">{{ session('success') }}</div>
+                @endif
+                
                 <form action="{{ route('student.photo.update') }}" method="POST" enctype="multipart/form-data">
                     @csrf
 
